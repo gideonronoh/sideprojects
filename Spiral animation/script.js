a951eb4b42f4dec351580d0b88faac4bcbf8973e
Gideon Rono <145061874+gideonronoh@users.noreply.github.com>
2024-03-10 23:55:29 +0300

    Spiral

---
+++ Spiral animation/script.js
@@ -0,0 +1,12 @@
+const words = "m";
+const ANIMATION_DURATION = 4000;
+const characters = words.split("").forEach((char,i) => {
+    function createElement(offset) {
+        const div = document.createElement("div");
+        div.innerText = char;
+        div.classList.add("character");
+        div.style.animationDelay = '${i* (ANIMATION_DURATION / 16)-offset}';
+        return div;
+    }
+    document.getElementById("spiral").append(createElement(0));
+});
